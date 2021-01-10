c9110887a87bbc2db24fd1ea99058369654f0f9d
Frank Hoedl <frankhoedl@mac.com>
2021-01-09 23:11:49 -0500

    javascipt notes & favicon

--- scripts.js
+++ scripts.js
@@ -1,28 +1,34 @@
-console.log('hello!');
-
 let fhApp = {};
+// Selectors
 fhApp.chooseWeDev = document.querySelector('.firsth1');
 fhApp.choosePhoto = document.querySelector('.secondh1');
 
+// Functions
 fhApp.Choose = () => {
-    fhApp.chooseWeDev.addEventListener('click', function (e) {
-        console.log('frank');
-    });
-    fhApp.choosePhoto.addEventListener('click', (e) => {
-        console.log('hoedl');
-    });
-}
-//Initialize 
+    fhApp.chooseWeDev.addEventListener('click', e => console.log('frank'));
+    fhApp.choosePhoto.addEventListener('click', e => console.log('hoedl'));
+};
+
+//Initialize
 fhApp.init = () => {
     fhApp.Choose()
-    // console.log('Welcome Frank Hoedl')	   
-}
+    console.log('Welcome Frank Hoedl')
+};
 
 // Document Ready
 let ready = (callback) => {
     if (document.readyState != "loading") callback();
     else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    fhApp.init();   
-});
+};
+ready(() => fhApp.init());
+
+
+// create function to determine position of the cursor
+// if on the left choose past - photography
+// if on the right choose future - web development
+// instead of click function respond on hover
+// animation to cover most of the screen on side or another
+    // create css for new background selection
+    // 
+// leave a portion to access the other
+// 
